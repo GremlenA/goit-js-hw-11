2073c43a921228dde7a5ec75a1458d128121d4d3
GremlenA <pedenkoartem5@gmail.com>
2025-08-23 09:43:00 +0300

    11

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -6,6 +6,7 @@ let lightbox = new SimpleLightbox(".gallery a", {
   captionDelay: 250,
 });
 
+
 export function renderGallery(images) {
   const gallery = document.querySelector(".gallery");
   if (!gallery) {
@@ -13,42 +14,66 @@ export function renderGallery(images) {
     return;
   }
 
-  const cards = images.map(img => `
+  const cards = images
+    .map(
+      (img) => `
     <li class="gallery-item">
       <a href="${img.largeImageURL}">
-        <img src="${img.webformatURL}" alt="${img.tags}">
+        <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy">
       </a>
       <div class="info">
-        <p>Likes: ${img.likes}</p>
-        <p>Views: ${img.views}</p>
-        <p>Comments: ${img.comments}</p>
-        <p>Downloads: ${img.downloads}</p>
+        <p><b>Likes:</b> ${img.likes}</p>
+        <p><b>Views:</b> ${img.views}</p>
+        <p><b>Comments:</b> ${img.comments}</p>
+        <p><b>Downloads:</b> ${img.downloads}</p>
       </div>
     </li>
-  `).join('');
+  `
+    )
+    .join("");
 
   gallery.insertAdjacentHTML("beforeend", cards);
 
+  
   lightbox.refresh();
 }
+
 
 export function clearGallery() {
   const gallery = document.querySelector(".gallery");
   if (gallery) {
-    gallery.innerHTML = '';
+    gallery.innerHTML = "";
   }
 }
+
 
 export function showLoader() {
   const loader = document.querySelector(".loader");
   if (loader) {
-    loader.classList.add("visible");
+    loader.classList.remove("hidden");
   }
 }
+
 
 export function hideLoader() {
   const loader = document.querySelector(".loader");
   if (loader) {
-    loader.classList.remove("visible");
+    loader.classList.add("hidden");
+  }
+}
+
+
+export function showLoadMoreButton() {
+  const btn = document.querySelector(".load-more");
+  if (btn) {
+    btn.classList.remove("hidden");
+  }
+}
+
+
+export function hideLoadMoreButton() {
+  const btn = document.querySelector(".load-more");
+  if (btn) {
+    btn.classList.add("hidden");
   }
 }
